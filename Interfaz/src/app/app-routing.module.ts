f14b011dd8c9c7b204f300731e98621e08b30d88
JesusJimenez4 <98493036+CatXplo1t@users.noreply.github.com>
2022-12-14 02:49:14 -0600

    Login interface, register interface, encuestas interfaces, inicio interface, navbar, boostrap and bootstrap icons

--- Interfaz/src/app/app-routing.module.ts
+++ Interfaz/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './Pages/login/login.component';
+import { InicioComponent } from './Pages/inicio/inicio.component';
+import { RegisterComponent } from './Pages/login/register/register.component';
+import { EncuestasComponent } from './Pages/encuestas/encuestas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  {path: "login", component:LoginComponent },
+  {path: "inicio", component:InicioComponent},
+  {path: "registro", component:RegisterComponent},
+  {path: "encuestas", component:EncuestasComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Interfaz/src/app/app.module.ts
+++ Interfaz/src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './Pages/inicio/inicio.component';
+import { LoginComponent } from './Pages/login/login.component';
+import { NavBarComponent } from './Component/nav-bar/nav-bar.component';
+import { FormsModule } from '@angular/forms';
+import { RegisterComponent } from './Pages/login/register/register.component';
+import { EncuestasComponent } from './Pages/encuestas/encuestas.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    LoginComponent,
+    NavBarComponent,
+    RegisterComponent,
+    EncuestasComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
